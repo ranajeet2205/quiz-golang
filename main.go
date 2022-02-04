32f90a50be416d82d3d3892a565616a13f274b64
Ranajeet <ranajeet.barik@r-softsolutions.com>
2022-02-04 22:23:45 +0530

    Adding Timer with time duration and goroutines

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	csvFileName := flag.String("csv", "problem.csv", "CSV File with Question And Answer")
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds")
 
 	flag.Parse()
 	file, err := os.Open(*csvFileName)
@@ -24,17 +26,31 @@ func main() {
 	}
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correct := 0
 
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("You Scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 	fmt.Printf("You Scored %d out of %d.\n", correct, len(problems))
+
 }
 
 func parseLines(lines [][]string) []problem {
